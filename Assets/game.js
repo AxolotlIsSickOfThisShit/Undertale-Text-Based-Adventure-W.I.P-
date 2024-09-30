f489876ff4fef1085b53c7810f091015cbd88141
Axolotl <sonicvsxander@gmail.com>
2024-09-29 19:08:51 -0500

    Added many new stats, added ITEMs and EQUIPMENT, added calculations for most stats, added the ability to equip stuff, granted the player the "Light Keyboard" at the start of the game, and more!

--- Assets/game.js
+++ Assets/game.js
@@ -23,6 +23,41 @@ function handleInput(input) {
 
 };
 
+function calcATK(lv,bonus) {
+    let atk = 6 + lv * 4
+    return atk + bonus;
+}
+
+function calcDEF(lv,bonus) {
+    let def = 7 + lv * 3
+    return def + bonus;
+}
+
+function calcHP(lv) {
+    let hp = hp + Math.floor(lv * 3.2)
+    return hp;
+}
+
+function calcMAXHP(lv) {
+    let  maxhp = 20 + Math.floor(lv * 3.2)
+    return maxhp;
+}
+
+function lvUP() {
+    let lv = gameState.lv;
+    let exp = gameState.exp;
+    if (Math.floor(exp / 150 * lv) >= lv) {
+        lv++
+        gameState.def = calcDEF(lv,gameState.bonusdf)
+        gameState.atk = calcATK(lv,gameState.bonusat)
+        gameState.hp = calcHP(lv);
+        gameState.maxhp = calcMAXHP(lv);
+        gameState.exp = 0;
+        gameState.lv = lv
+        appendOutput(`Your LOVE increased to ${lv}!`)
+    }
+}
+
 function displayLocation() {
     const location = locations[gameState.currentLocation];
     appendOutput(location.description);
@@ -43,17 +78,28 @@ document.getElementById('input').addEventListener('keypress', function (event) {
     }
 });
 
+function onBattleWin(exp,G) {
+    gameState.exp =  gameState.exp + exp;
+    gameState.g = gameState.g + G;
+    lvUP();
+}
+
 // DO NOT MODIFY ANYTHING ABOVE THIS COMMENT!!! --------------------------------------------------------
 
 const gameState = {
     currentLocation: 'firstLocation', // Change this value to change the room you start in!
-    inventory: ["stick", "bandage"], // Add things inside this list to add them to your inventory!
+    inventory: ["Light Keyboard"], // Add things inside this list to add them to your inventory!
     hp:20,
     maxhp:20,
     lv:1,
     atk:1,
+    bonusat: 0,
     def:1,
-
+    bonusdf:0,
+    armor: "bandage",
+    weapon: "stick",
+    exp:0,
+    g:0
 }; // Modify this const to add things to the game logic!
 
 const locations = {
@@ -69,8 +115,76 @@ const locations = {
         description: "You are in a large room with a doorway at the end of it.<br>There's a small patch of grass in the middle of the room."
     }
 }; // Expand and modify this list to add more locations.
+const items = {
+    "Monster Candy": {
+        description: "A piece of candy that heals 5 HP.<br>* Does not taste like black licorice.",
+        h:5,
+        onUse: (amount) => {
+            appendOutput("You eat the Monster Candy.<br>* It actually doesn't taste like licorice.");
+            appendOutput(`You healed ${amount} HP.`);
+            // Here you can add more effects.
+        }
+    },
+    "Spider Donut": {
+        description: "A donut made by spiders, for spiders...<br>* Of spiders! (Heals 9 HP.)",
+        h:9,
+        onUse: (amount) => {
+            appendOutput("You ate the Spider Donut. Not bad for a bunch of arthropods!");
+            appendOutput(`You healed ${amount} HP.`);
+            // Here you can add more effects.
+            appendOutput("A spider notices you eating the donut and thanks you.")
+        }
+    }
+}// List of items. Items MUST be in this list or the equipment list, or the game won't recognize them.
+
+const equipment = {
+    Bandage: {
+        description: "A worn bandage that heals 5 HP when used.<br>* Not very effective.",
+        h:5,
+        onUse: () => {
+            appendOutput("You reapply the bandage to heal yourself.");
+            appendOutput(`You healed ${h} HP.`);
+        }
+    },
+    Stick: {
+        description: "A stick that can be used as a weapon.<br>* Not very effective.",
+        a:0,
+        onUse: () => {
+            appendOutput("You equip the stick.");
+            appendOutput("You are now holding a stick.");
+            gameState.bonusat = a
+            let atk = calcATK(gameState.lv,a) 
+            appendOutput(`Your ATK is now ${atk}.`)
+            gameState.atk = atk
+        }
+    },
+    "Light Keyboard": {
+        description: "A lightweight keyboard.<br>* Could be used as a weapon.",
+        a:2,
+        onUse:()=>{
+            appendOutput("You equip the keyboard.");
+            appendOutput("It makes a clacking sound as you do.");
+            gameState.bonusat = a
+            let atk = calcATK(gameState.lv,a) 
+            appendOutput(`Your ATK is now ${atk}.`)
+            gameState.atk = atk
+        }
+    }
+}
 
 const cmd = {
+    use: (i)=>{
+        if (i in items | i in equipment) {
+            if (i in inventory) {
+                i.onUse();
+            } else {
+                appendOutput("You don't have that item.");
+            }
+        } else {
+            appendOutput(`"${i}" is not a valid item. Check your capitalization and spelling, and try again.`)
+        }
+    },
+
     items: () => {
         appendOutput(`You have: ${gameState.inventory.join(", ")}`);
     },
@@ -148,6 +262,9 @@ const alias = {
     },
     clr: () => {
         cmd.clear();
+    },
+    equip:(i)=>{
+        cmd.use(i);
     }
 }; // This const defines aliases. To add one, simply follow the format of the ones above.
 
